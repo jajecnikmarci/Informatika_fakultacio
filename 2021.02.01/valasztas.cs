ed3e1f6c9ec8e28e0e97df72837463cd440a06cf
jajecnikmarci <63594328+jajecnikmarci@users.noreply.github.com>
2021-02-15 19:12:18 +0100

    Update valasztas.cs

--- 2021.02.01/valasztas.cs
+++ 2021.02.01/valasztas.cs
@@ -19,6 +19,7 @@ namespace _2021._02._01
     class Program
     {
         static int lakossag = 12345;
+        static int szavazo = 0;
         static adat[] t = new adat[100];
         static int n = 0;
         static keres[] k = new keres[1];
@@ -75,27 +76,124 @@ namespace _2021._02._01
         static void feladat4()
         {
             Console.WriteLine("4.feladat");
-            int b = 0;
             float c = 5;
             for (int i = 0; i < n; i++)
             {
-                b += t[i].szavazat;
+                szavazo += t[i].szavazat;
             }
-            c = (float)b / (float)lakossag;
+            c = (float)szavazo / (float)lakossag;
             c =(float)Math.Round(c * 100, 2);
-            Console.WriteLine("A választáson "+b+" állampolgár, a jogosultak "+c+"%-a vett részt.");
+            Console.WriteLine("A választáson "+szavazo+" állampolgár, a jogosultak "+c+"%-a vett részt.");
         }
         static void feladat5()
         {
             Console.WriteLine("5.feladat");
+            int g = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i].part=="GYEP")
+                {
+                    g += t[i].szavazat;
+                }
+            }
+            double gszazalek = (double)g / szavazo*100;
+            gszazalek = (double)Math.Round(gszazalek, 2);
+            Console.WriteLine("Gyümölcsevők Pártja= "+gszazalek+"%");
+            int h = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i].part == "HEP")
+                {
+                    h += t[i].szavazat;
+                }
+            }
+            double hszazalek = (double)h / szavazo * 100;
+            hszazalek = (double)Math.Round(hszazalek, 2);
+            Console.WriteLine("Húsevők Pártja= " + hszazalek + "%");
+            int te = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i].part == "TISZ")
+                {
+                    te += t[i].szavazat;
+                }
+            }
+            double teszazalek = (double)te / szavazo * 100;
+            teszazalek = (double)Math.Round(teszazalek, 2);
+            Console.WriteLine("Tejivók Szövetsége= " + teszazalek + "%");
+            int z = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i].part == "ZEP")
+                {
+                    z += t[i].szavazat;
+                }
+            }
+            double zszazalek = (double)z / szavazo * 100;
+            zszazalek = (double)Math.Round(zszazalek, 2);
+            Console.WriteLine("Zöldségevők Pártja= " + zszazalek + "%");
+            int f = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i].part == "-")
+                {
+                    f += t[i].szavazat;
+                }
+            }
+            double fszazalek = (double)f / szavazo * 100;
+            fszazalek = (double)Math.Round(fszazalek, 2);
+            Console.WriteLine("Független jelöltek= " + fszazalek + "%");
         }
         static void feladat6()
         {
             Console.WriteLine("6.feladat");
+            int max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i].szavazat>max)
+                {
+                    max = t[i].szavazat;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (t[i].szavazat==max)
+                {
+                    if (t[i].part=="-")
+                    {
+                        Console.WriteLine(t[i].vez_nev+" "+ t[i].ker_nev+" Független");
+                    }
+                    else
+                    {
+                        Console.WriteLine(t[i].vez_nev + " " + t[i].ker_nev+" "+t[i].part);
+                    }
+                }
+            }
         }
         static void feladat7()
         {
             Console.WriteLine("7.feladat");
+            StreamWriter ki = new StreamWriter("kepviselok.txt");
+            for (int ker = 1; ker <=8 ; ker++)
+            {
+                int max = 0;
+                int s = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (t[i].ker==ker)
+                    {
+                        if (t[i].szavazat>max)
+                        {
+                            max = t[i].szavazat;
+                            s = i;
+                        }
+                    }
+                }
+                if (t[s].part=="-")
+                {
+//                    ki.WriteLine(ker+"")
+                }
+            }
         }
         static void Main(string[] args)
         {
